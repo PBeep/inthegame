7cfe1221b9480d65090e4f96d2a9b162b58afca4
Pranoti <pranotibhonsle@gmail.com>
2022-06-15 18:44:14 +0100

    added functionality to make popup disappear by clicking anywhere on the background. Added an overlay when popup is active

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
  // When the user clicks on div, open the popup
- function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+
+//select popup with id, assign to variable to be manipulated
+const popup = document.querySelector('#myPopup');
+const overlay = document.getElementById('overlay');
+ //function to detect clicks
+function myFunction(event) {
+    popup.classList.add('show');
+    overlay.classList.add('active');
 }
   
-document.addEventListener('click', function handleClickOutsideBox(event) {
-    const box = document.querySelector("show");
-    if (!box.contains(event.target)) {
-      box.style.display = 'none';
-    }
-  });
+overlay.addEventListener('click', function() {
+    closePopup(popup);
+})
+
+
+function closePopup(popup) {
+    popup.classList.remove('show');
+    overlay.classList.remove('active');
+}
+
+
+// document.addEventListener('click', function handleClickOutsideBox(event) {
+//     const box = document.querySelector("myPopup");
+//     if (!box.contains(event.target)) {
+//       box.style.display = 'none';
+//     }
+//   });
